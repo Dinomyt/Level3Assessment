d00e671a70e30296216457a900a31f1f463e78c9
Dinomyt <149448460+Dinomyt@users.noreply.github.com>
2024-06-25 20:22:40 -0700

    +init

---
+++ lvl3-assessment/src/hooks/myHook.ts
@@ -0,0 +1,14 @@
+// Get the data from this url endpoint https://jsonplaceholder.typicode.com/
+import axios from "axios"
+import { useEffect } from "react"
+
+
+const myHook = () => {
+    useEffect(() => {
+      
+
+    }, [])
+    
+}
+
+export default myHook
